027177c6effb374e0c0703807a876314d0d569ed
Jeremy Myers <echelonical@gmail.com>
2022-10-14 19:42:25 -0500

    updated code so kennel-client can POST new data to database

--- kennel.sql
+++ kennel.sql
@@ -133,3 +133,7 @@ SELECT
 FROM Employee e
 JOIN Location l
 	ON l.id = e.location_id
+
+SELECT * 
+FROM Animal
+ORDER BY id DESC;
--- views/animal_requests.py
+++ views/animal_requests.py
@@ -152,21 +152,32 @@ def get_single_animal(id):
 
         return animal.__dict__
 
-def create_animal(animal):
-    # Get the id value of the last animal in the list
-    max_id = ANIMALS[-1]["id"]
 
-    # Add 1 to whatever that number is
-    new_id = max_id + 1
+def create_animal(new_animal):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
 
-    # Add an `id` property to the animal dictionary
-    animal["id"] = new_id
+        db_cursor.execute("""
+        INSERT INTO Animal
+            ( name, breed, status, location_id, customer_id )
+        VALUES
+            ( ?, ?, ?, ?, ?);
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], ))
 
-    # Add the animal dictionary to the list
-    ANIMALS.append(animal)
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
 
-    # Return the dictionary with `id` property added
-    return animal
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_animal['id'] = id
+
+
+    return new_animal
 
 def delete_animal(id):
     with sqlite3.connect("./kennel.sqlite3") as conn:
@@ -298,3 +309,20 @@ def get_animal_by_status(status):
 #     # If the animal was found, use pop(int) to remove it from list
 #     if animal_index >= 0:
 #         ANIMALS.pop(animal_index)
+
+
+# def create_animal(animal):
+#     # Get the id value of the last animal in the list
+#     max_id = ANIMALS[-1]["id"]
+
+#     # Add 1 to whatever that number is
+#     new_id = max_id + 1
+
+#     # Add an `id` property to the animal dictionary
+#     animal["id"] = new_id
+
+#     # Add the animal dictionary to the list
+#     ANIMALS.append(animal)
+
+#     # Return the dictionary with `id` property added
+#     return animal
